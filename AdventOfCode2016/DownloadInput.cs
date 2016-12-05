f6ffe2bbb9826fe106dff7f083e016fc6a661a7f
Kevin Sijbers <k.sijbers@gmail.com>
2016-12-05 07:45:02 +0100

    Download Input

---
+++ AdventOfCode2016/DownloadInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+    public static class DownloadInput
+    {
+        private static string SessionId = "removed_for_github";
+        public static string GetInput(int day, int puzzle)
+        {
+            string output = "";
+            using(WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("Cookie", "session="+SessionId);
+                output = wc.DownloadString("http://adventofcode.com/2016/day/" + day + "/input");
+            }
+            return output;
+        }
+    }
+}
