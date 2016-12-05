363e0a86b14df4fbf8a50b2f1ed5dce6e7f41c14
Kevin Sijbers <k.sijbers@gmail.com>
2016-12-05 08:43:50 +0100

    Day1

---
+++ AdventOfCode2016/Code/Day1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdventOfCode2016.Code
+{
+    public class Day1
+    {
+        public string Puzzle1(string overrideInput = "")
+        {
+            string input = "";
+            if (overrideInput != "")
+                input = overrideInput;
+            else
+                input = DownloadInput.GetInput(1);
+            string[] instructions = input.Replace("\n", "").Split(',');
+            Point position = new Point(0, 0);
+            string currentDirection = "U";
+            foreach (string instruction in instructions)
+            {
+                ApplyInstruction(ref position, ref currentDirection, instruction);
+            }
+            return (Math.Abs(position.X) + Math.Abs(position.Y)).ToString();
+        }
+
+        public string Puzzle2(string overrideInput = "")
+        {
+            string input = "";
+            if (overrideInput != "")
+                input = overrideInput;
+            else
+                input = DownloadInput.GetInput(1);
+            string[] instructions = input.Replace("\n", "").Split(',');
+            Point position = new Point(0, 0);
+            string currentDirection = "U";
+            List<Point> visited = new List<Point>();
+            Point lastPosition = new Point(0, 0);
+            foreach (string instruction in instructions)
+            {
+                lastPosition = new Point(position.X, position.Y);
+                ApplyInstruction(ref position, ref currentDirection, instruction);
+                List<Point> newPoints = new List<Point>();
+                bool first = true;
+                int xSign = Math.Sign(position.X - lastPosition.X) >= 0 ? 1 : -1;
+                int ySign = Math.Sign(position.Y - lastPosition.Y) >= 0 ? 1 : -1;
+                for (int x = (int)lastPosition.X; xSign > 0 ? (x <= position.X) : (x >= position.X); x = x + xSign)
+                {
+                    for (int y = (int)lastPosition.Y; ySign > 0 ? (y <= position.Y) : (y >= position.Y); y = y + ySign)
+                    {
+                        if (first)
+                            first = false;
+                        else
+                            newPoints.Add(new Point(x, y));
+                    }
+                }
+                if (newPoints.Any(p => visited.Any(p2 => p2.X == p.X && p2.Y == p.Y)))
+                {
+                    position = newPoints.First(p => visited.Any(p2 => p2.X == p.X && p2.Y == p.Y));
+                    break;
+                }
+                visited = visited.Concat(newPoints).ToList();
+            }
+            return (Math.Abs(position.X) + Math.Abs(position.Y)).ToString();
+        }
+
+        private static void ApplyInstruction(ref Point position, ref string currentDirection, string instruction)
+        {
+            string direction = instruction.Trim().Substring(0, 1);
+            currentDirection = direction == "L" ? (currentDirection == "U" ? "L" : currentDirection == "L" ? "D" : currentDirection == "D" ? "R" : "U") : (currentDirection == "U" ? "R" : currentDirection == "L" ? "U" : currentDirection == "D" ? "L" : "D");
+            int amount = int.Parse(instruction.Trim().Substring(1));
+            switch (currentDirection)
+            {
+                case "L":
+                    position.X -= amount;
+                    break;
+                case "U":
+                    position.Y -= amount;
+                    break;
+                case "R":
+                    position.X += amount;
+                    break;
+                case "D":
+                    position.Y += amount;
+                    break;
+            }
+        }
+    }
+}
--- AdventOfCode2016/MainWindow.xaml.cs
+++ AdventOfCode2016/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode2016.Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,9 +26,22 @@ namespace AdventOfCode2016
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void btn_Day1_1_Click(object sender, RoutedEventArgs e)
         {
-            string s = DownloadInput.GetInput(1, 1);
+            Day1 day = new Day1();
+            string test1 = day.Puzzle1("R2, L3");
+            string test2 = day.Puzzle1("R2, R2, R2");
+            string test3 = day.Puzzle1("R5, L5, R5, R3");
+            string puzzle1 = day.Puzzle1();
+            MessageBox.Show(puzzle1);
+        }
+
+        private void btn_Day1_2_Click(object sender, RoutedEventArgs e)
+        {
+            Day1 day = new Day1();
+            string test4 = day.Puzzle2("R8, R4, R4, R8");
+            string puzzle2 = day.Puzzle2();
+            MessageBox.Show(puzzle2);
         }
     }
 }
